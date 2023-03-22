b08a79130fc9425ae1240b0e513eb3d9284657e0
CONFIZ\abdul.qadeer <abdul.qadeer@confiz.com>
2023-03-22 18:52:29 +0500

    update

---
+++ preprocessing.py
@@ -0,0 +1,25 @@
+from sklearn.model_selection import train_test_split
+class Preprocessing():
+    def __init__(self):
+        print("Preprocessing called")
+
+    def data_aggregation(self,df):
+        print("Data aggregation called")
+
+        return df
+
+    def mark_zero(self,df):
+        print("Marking zero called")
+
+        return df
+
+    def impute_mean(self,df):
+        print("ImputeMean called")
+        
+        return df
+
+    def train_test_split(self,data,target):
+        print("Train test split called")
+        X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.33, random_state=42)
+
+        return X_train, X_test, y_train, y_test
