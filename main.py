1e1796556afe619590a6b8a03ee7e29cb8a36f9a
CONFIZ\abdul.qadeer <abdul.qadeer@confiz.com>
2023-03-27 10:06:40 +0500

    new updated files

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 from preprocessing import Preprocessing
+import pandas as pd
+
+df =pd.read_csv("./sales_train_validation.csv")
+calendar = pd.read_csv('./calendar.csv')
 
--- preprocessing.py
+++ preprocessing.py
@@ -1,12 +1,14 @@
 from sklearn.model_selection import train_test_split
+
 class Preprocessing():
     def __init__(self):
         print("Preprocessing called")
 
     def data_aggregation(self,df):
+        sales_store_agg = df.groupby('store_id','dept_id').sum()
         print("Data aggregation called")
 
-        return df
+        return sales_store_agg
 
     def mark_zero(self,df):
         print("Marking zero called")
