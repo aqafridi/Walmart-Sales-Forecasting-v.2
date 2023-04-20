367b297e7dc242862661b66f622012a9bda67851
aqafridi <imaqafridi@gmail.com>
2023-04-20 10:28:45 +0500

    update
    Signed-off-by: aqafridi <imaqafridi@gmail.com>

--- main.py
+++
@@ -1,9 +0,0 @@
-# import hydra
-# from omegaconf import DictConfig, OmegaConf
-
-# @hydra.main(version_base=None, config_path="configs", config_name="hydra")
-# def my_app(cfg : DictConfig) -> None:
-#     print(OmegaConf.to_yaml(cfg))
-
-# if __name__ == "__main__":
-#     my_app()
